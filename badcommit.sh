eee888acace7d26e292d928f247b09b2ab2453fe
ahsaan ali <ahsaanali476@gmail.com>
2022-03-31 06:41:09 +0530

    updated and new files

---
+++ badcommit.sh
@@ -0,0 +1,1 @@
+when u commit is bad
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+this lines are created for work
+when u edit the file again for practice
+
 this is my file ahsaan
 this is the updated line
 this is the first line to add the code
