1f6260f8fdb1ce572409d1aba3ed1de252d77501
ahsaan ali <ahsaanali476@gmail.com>
2022-04-02 14:50:06 +0530

    deleter lines

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,2 @@
-this is for editing cherry
 
-this is for cherry picking
-
-this code is updated in dev branch"
-this is my file ahsaan
-this is the updated line
-this is the first line to add the code
-this line is again updated
-this is updating for stash testing
+this are deleted
