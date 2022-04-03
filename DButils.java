448e227ea322eadeee0e17ea5098661ad5aec06b
Ahsaan ali <97252966+Muzzammil476@users.noreply.github.com>
2022-04-03 15:15:59 +0530

    Update DButils.java

--- DButils.java
+++ DButils.java
@@ -2,3 +2,5 @@ hi this is to practice pull request
 let us know about pull 
 updated code is 
 hi this is the first line 
+hi iam updating this for fetch cmd 
+so after fecth we have to use merge cmd
